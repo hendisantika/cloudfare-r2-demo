a8d9927e59e6ef18e36390d9cbfc84ce70a220c8
hendisantika <hendisantika@yahoo.co.id>
2024-12-08 15:24:07 +0700

    Modify S3Config🫸🌀✏️📗🐧🐳⬆

---
+++ backend/src/main/java/id/my/hendisantika/backend/dto/StorageConfig.java
@@ -0,0 +1,47 @@
+package id.my.hendisantika.backend.dto;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cloudfare-r2-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/12/24
+ * Time: 10.13
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "aws")
+public class StorageConfig {
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.access-secret}")
+    private String accessSecret;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, accessSecret);
+        return S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .region(Region.of(region))
+                .build();
+    }
+
+}
