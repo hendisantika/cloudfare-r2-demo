b0f436f0cfe66b1501e34da72cd292954f5b4d84
hendisantika <hendisantika@yahoo.co.id>
2024-12-08 15:36:14 +0700

    Add uploadFile2 in FileUploadController🫸🌀✏️📗🐧🐳⬆

--- backend/src/main/java/id/my/hendisantika/backend/service/FileUploadService.java
+++ backend/src/main/java/id/my/hendisantika/backend/service/FileUploadService.java
@@ -3,12 +3,13 @@ package id.my.hendisantika.backend.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
-import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,10 +31,26 @@ public class FileUploadService {
     @Value("${aws.s3.bucket}")
     private String bucketName;
 
-    public void uploadFile(String keyName, MultipartFile file) throws IOException {
-        PutObjectResult putObjectResult = s3Client.putObject(bucketName, keyName, file.getInputStream(), null);
-        log.info(putObjectResult.getMetadata());
+//    public void uploadFile(String keyName, MultipartFile file) throws IOException {
+//        PutObjectResult putObjectResult = s3Client.putObject(bucketName, keyName, file.getInputStream(), null);
+//        log.info(putObjectResult.getMetadata());
+//    }
+
+    public String uploadFile(ByteArrayResource resource, String fileName) {
+        PutObjectRequest putObjectRequest =
+                PutObjectRequest.builder().bucket(bucketName).key(fileName).build();
+        try (InputStream inputStream = resource.getInputStream()) {
+            s3Client.putObject(
+                    putObjectRequest,
+                    software.amazon.awssdk.core.sync.RequestBody.fromInputStream(
+                            inputStream, resource.contentLength()));
+            log.info("Upload File success: {}", fileName);
+        } catch (Exception e) {
+            log.error("Error while uploading to s3 file {} with {}", e, fileName);
+        }
+        return fileName;
     }
+
 
     public S3Object getFile(String keyName) {
         try {
