d25fd0a1e0abd2c4fa102213476e60ad4a3c2d67
hendisantika <hendisantika@yahoo.co.id>
2024-12-08 15:34:18 +0700

    Add downloadFile in FileUploadController🫸🌀✏️📗🐧🐳⬆

--- backend/src/main/java/id/my/hendisantika/backend/controller/FileUploadController.java
+++ backend/src/main/java/id/my/hendisantika/backend/controller/FileUploadController.java
@@ -3,7 +3,12 @@ package id.my.hendisantika.backend.controller;
 import id.my.hendisantika.backend.service.FileUploadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +35,12 @@ public class FileUploadController {
     public String status() {
         log.info("{} OK", LocalDateTime.now());
         return LocalDateTime.now() + "OK";
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) throws AmazonS3Exception {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(new InputStreamResource(fileUploadService.getFile(fileName).getObjectContent()));
     }
 }
